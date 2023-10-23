370fdc8c22734aeedb093a8fa18ae19995102273
Nikita Tkachov <nikita.tka4ov@gmail.com>
2023-10-23 20:48:43 +0300

    fix: show scrollbar permanently on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Main() {
                 }
               : {
                   modules: [Scrollbar],
-                  scrollbar: { hide: true },
+                  scrollbar: { hide: false },
                   slidesPerView: "auto",
                 })}
           />
